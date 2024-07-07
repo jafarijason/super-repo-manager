5a5acaf8847044883a50caea5739829b29b82b33
Jason Jafari <me@jasonjafari.com>
2024-07-07 03:06:43 -0400

    v1.0.29

--- src/functions/reposUtils.ts
+++ src/functions/reposUtils.ts
@@ -41,7 +41,6 @@ export const doesDirectoryExist = (dirPath) => {
 }
 
 export const ensureRepo = async (repo) => {
-    console.log(repo)
     const [repoObj] = await Promise.all([
         getRepo(repo),
     ])
